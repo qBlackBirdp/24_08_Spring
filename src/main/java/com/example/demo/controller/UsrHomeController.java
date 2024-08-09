58c4aa9111ef66953578636b179ba266cee57955
qBlackbirdp <blackbird9711@gmail.com>
2024-08-09 13:07:26 +0900

    articleController, doAdd, getArticles 추가

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -9,21 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 @Controller
 public class UsrHomeController {
-
-	@RequestMapping("/usr/home/getArticle")
-	@ResponseBody
-	public Article getArticle() {
-
-		Article article = new Article(1, "제목1", "내용1");
-
-		return article;
-	}
 
 	@RequestMapping("/usr/home/getList")
 	@ResponseBody
@@ -70,12 +57,3 @@ public class UsrHomeController {
 
 }
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Article {
-
-	int id;
-	String title;
-	String body;
-}
