8614536a510e00a7ca49274cc5344871850c0334
qBlackbirdp <blackbird9711@gmail.com>
2024-08-11 19:38:06 +0900

    Repository 도입

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -3,21 +3,25 @@ package com.example.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.repository.ArticleRepository;
 import com.example.demo.vo.Article;
 
 @Service
 public class ArticleService {
 
+	@Autowired
+	private ArticleRepository articleRepository;
+
 	private int lastArticleId;
 	public List<Article> articles;
 
-	public ArticleService() {
-		lastArticleId = 0;
-		articles = new ArrayList<>();
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
 
 		makeTestData();
 	}
@@ -28,38 +32,30 @@ public class ArticleService {
 			String title = "제목" + i;
 			String body = "내용" + i;
 
-			writeArticle(title, body);
+			articleRepository.writeArticle(title, body);
 		}
 	}
 
 	public Article writeArticle(String title, String body) {
-		int id = lastArticleId + 1;
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++;
-		return article;
+		return articleRepository.writeArticle(title, body);
 	}
 
 	// 입력된 id와 일치하는 article 찾기
 	@RequestMapping("/usr/article/findById")
 	@ResponseBody
 	public Article findById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
+		return articleRepository.getArticleById(id);
 	}
 
 	public void modifyArticle(int id, String title, String body) {
-		Article article = findById(id);
-		article.setTitle(title);
-		article.setBody(body);
+		articleRepository.modifyArticle(id, title, body);
 	}
 
 	public void deleteArticle(int id) {
-		Article article = findById(id);
-		articles.remove(article);
+		articleRepository.deleteArticle(id);
+	}
+	
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
 	}
 }
