ae79654ed90b1fd8adfa316db1e51f7b36513517
qBlackbirdp <blackbird9711@gmail.com>
2024-08-11 18:51:24 +0900

    게시물 삭제 수정

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -20,7 +20,7 @@ public class UsrArticleController {
 		articles = new ArrayList<>();
 	}
 
-	@RequestMapping("/usr/article/doAdd") 
+	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = lastArticleId + 1;
@@ -35,5 +35,60 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
+
+	// 테스트데이터 생성.
+	@RequestMapping("/usr/article/getTestArticle")
+	@ResponseBody
+	public List<Article> getArticle() {
+	    
+	    for (int i = 1; i <= 10; i++) {
+	        Article article = new Article(i, "제목" + i, "내용" + i);
+	        articles.add(article);
+	    }
+	    
+	    return articles;
+	}
+	// 입력된 id와 일치하는 article 찾기
+	@RequestMapping("/usr/article/findById")
+	@ResponseBody
+    public Article findById(int id) {
+        for (Article article : articles) {
+            if (article.getId() == id) {
+                return article;
+            }
+        }
+        return null;
+    }
+    //게시물 삭제.
+    @RequestMapping("/usr/article/doDeleteArticle")
+    @ResponseBody
+    public  String DoDeleteArticle(int id) {
+    	Article article = findById(id);
+    	
+    	if (article == null) {
+    		return id + "번 게시물 존재하지 않음.";
+    	} else articles.remove(article);
+    	
+    	
+    	return id + "번 게시물 삭제.";
+    }
+    
+    //게시물 수정.
+    @RequestMapping("/usr/article/doModifyArticle")
+    @ResponseBody
+    public  String DoModifyArticle(int id, String newTitle, String newBody) {
+    	Article article = findById(id);
+    	
+    	if (article == null) {
+    		return id + "번 게시물 존재하지 않음.";
+    	} else {
+    		article.setTitle(newTitle);
+    		article.setBody(newBody);
+    	}
+    	//doModifyArticle?id=1%newTitle=%EC%83%88%EC%A0%9C%EB%AA%A9&newBody=%EC%83%88%EB%82%B4%EC%9A%A9
+    	
+    	return id + "번 게시물 수정.";
+    }
+    
 
 }
--- src/main/java/com/example/demo/vo/Article.java
+++ src/main/java/com/example/demo/vo/Article.java
@@ -1,5 +1,7 @@
 package com.example.demo.vo;
 
+import java.util.Objects;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +14,17 @@ public class Article {
 	int id;
 	String title;
 	String body;
+	
+//	@Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Article article = (Article) o;
+//        return id == article.id; // id로 동일성 판단
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(id);
+//    }
 }
